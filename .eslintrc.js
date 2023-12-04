5fca3b4ea200d1306243b1b3377102394556051a
Diego Alzate <diego35.da@gmail.com>
2023-12-04 16:16:07 -0500

    initial config for forum

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_ignored' },
+        ],
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "tabWidth": 4,
+    "singleQuote": true,
+    "quoteProps": 'consistent',
+    "printWidth": 100,
+}
