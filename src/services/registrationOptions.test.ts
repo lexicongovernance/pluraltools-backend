0fb3af457b79ca786e38fbb81557d069fc7a1724
Diego Alzate <diego35.da@gmail.com>
2023-12-18 12:40:46 -0500

    remove console log tests

--- src/services/registrationOptions.test.ts
+++ src/services/registrationOptions.test.ts
@@ -23,55 +23,37 @@ describe('service: registrationOptions', function () {
 
     // Insert registration options for the user
     const registrationOptions = ['option1', 'option2', 'option3'];
-    const userRegistrationOptions = registrationOptions.map((registrationOptionId) => ({
-      id: registrationOptionId,
-      name: 'Example Name',
+    const dbRegistrationOptions = registrationOptions.map((registrationOptionId) => ({
+      name: registrationOptionId,
       category: 'Example Category',
-      created_at: new Date(),
-      updated_at: new Date(),
     }));
-    await dbPool.insert(db.registrationOptions).values(userRegistrationOptions).execute();
+    defaultRegistrations = await dbPool
+      .insert(db.registrationOptions)
+      .values(dbRegistrationOptions)
+      .returning();
   });
 
-  test('should throw an error if it tries to delete a non-existing record', async () => {
-    const userId = 'testUserId_0';
+  test('should save initial registration options', async () => {
+    const initialRegistrationOptions = defaultRegistrations.map((r) => r.id);
+    const result = await saveUsersToRegistrationOptions(dbPool, user?.id || '', [
+      initialRegistrationOptions[0] ?? '',
+    ]);
 
-    // Mock console.log to spy on its calls
-    const consoleLogSpy = jest.spyOn(console, 'log');
-
-    const result = await saveUsersToRegistrationOptions(dbPool, userId, []); // define empty options
-
-    // Check if result is null
-    expect(result).toBeNull();
-
-    // Check that an error message is logged (substring match)
-    expect(consoleLogSpy).toHaveBeenCalledWith(
-      expect.stringContaining('error deleting user registration options'),
-    );
-
-    // Restore the original console.log method to avoid interference with other tests
-    consoleLogSpy.mockRestore();
+    expect(result?.[0]).toBeDefined();
+    expect(result?.[0]?.userId).toBe(user?.id);
   });
 
-  test('should delete existing user data and log "Deletion successful."', async () => {
+  test('should overwrite registration options', async () => {
     // Insert some initial registration options for the user
-    const initialRegistrationOptions = ['option1', 'option2', 'option3'];
-    await saveUsersToRegistrationOptions(dbPool, user?.id || '', initialRegistrationOptions);
-
-    // Mock console.log to spy on its calls
-    const consoleLogSpy = jest.spyOn(console, 'log');
-
-    // Call the function to delete existing user data
-    const result = await saveUsersToRegistrationOptions(dbPool, user?.id || '', []);
+    const initialRegistrationOptions = defaultRegistrations.map((r) => r.id);
+    const result = await saveUsersToRegistrationOptions(dbPool, user?.id || '', [
+      initialRegistrationOptions[1] ?? '',
+    ]);
 
     // Check if result is not null (indicating success)
-    expect(result).toBeNull();
-
-    // Check that console.log is called with the expected message
-    expect(consoleLogSpy).toHaveBeenCalledWith('Deletion successful.');
-
-    // Restore the original console.log method to avoid interference with other tests
-    consoleLogSpy.mockRestore();
+    expect(result).toBeDefined();
+    expect(result?.[0]?.userId).toBe(user?.id);
+    expect(result?.[0]?.registrationOptionId).toBe(initialRegistrationOptions[1]);
   });
 
   afterAll(async function () {
@@ -79,6 +61,14 @@ describe('service: registrationOptions', function () {
     await dbPool
       .delete(db.usersToRegistrationOptions)
       .where(eq(db.usersToRegistrationOptions.userId, user?.id ?? ''));
+
+    // delete registration options
+    await dbPool.delete(db.registrationOptions).where(
+      inArray(
+        db.registrationOptions.id,
+        defaultRegistrations.map((r) => r.id),
+      ),
+    );
 
     // delete user
     await dbPool.delete(db.users).where(eq(db.users.id, user?.id ?? ''));
