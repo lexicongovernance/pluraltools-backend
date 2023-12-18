3ad59e484951eb8e956906c8c8a0fa0dfebc9d27
Martin Benedikt Busch <bene.busch1992@gmail.com>
2023-12-18 17:31:11 +0100

    add tests

--- src/services/registrationOptions.test.ts
+++ src/services/registrationOptions.test.ts
@@ -20,6 +20,17 @@ describe('service: registrationOptions', function () {
     dbPool = initDb.dbPool;
     dbConnection = initDb.connection;
     user = (await dbPool.insert(db.users).values({}).returning())[0];
+
+    // Insert registration options for the user
+  const registrationOptions = ['option1', 'option2', 'option3'];
+  const userRegistrationOptions = registrationOptions.map((registrationOptionId) => ({
+    id: registrationOptionId,
+    name: 'Example Name', 
+    category: 'Example Category',
+    created_at: new Date(), 
+    updated_at: new Date(), 
+  }));
+  await dbPool.insert(db.registrationOptions).values(userRegistrationOptions).execute();
   });
 
   test('should throw an error if it tries to delete a non-existing record', async () => {
@@ -42,7 +53,31 @@ describe('service: registrationOptions', function () {
     consoleLogSpy.mockRestore();
   });
 
+  test('should delete existing user data and log "Deletion successful."', async () => {
+    // Insert some initial registration options for the user
+    const initialRegistrationOptions = ['option1', 'option2', 'option3'];
+    await saveUsersToRegistrationOptions(dbPool, user?.id || '', initialRegistrationOptions);
+
+    // Mock console.log to spy on its calls
+    const consoleLogSpy = jest.spyOn(console, 'log');
+
+    // Call the function to delete existing user data
+    const result = await saveUsersToRegistrationOptions(dbPool, user?.id || '', []);
+
+    // Check if result is not null (indicating success)
+    expect(result).toBeNull();
+
+    // Check that console.log is called with the expected message
+    expect(consoleLogSpy).toHaveBeenCalledWith('Deletion successful.');
+
+    // Restore the original console.log method to avoid interference with other tests
+    consoleLogSpy.mockRestore();
+  });
+
   afterAll(async function () {
+    // Delete user registration options
+    await dbPool.delete(db.usersToRegistrationOptions).where(eq(db.usersToRegistrationOptions.userId, user?.id ?? ''));
+
     // delete user
     await dbPool.delete(db.users).where(eq(db.users.id, user?.id ?? ''));
     await dbConnection.end();
