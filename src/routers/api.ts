e8045d5d83f8c684187e718f101758b86de2fd1f
Diego Alzate <diego35.da@gmail.com>
2023-12-20 18:09:01 -0500

    add cycles to api router (#33)

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -8,6 +8,7 @@ import { usersRouter } from './users';
 import { registrationsRouter } from './registrations';
 import { groupsRouter } from './groups';
 import { registrationOptionsRouter } from './registrationOptions';
+import { cyclesRouter } from './cycles';
 
 const router = express.Router();
 
@@ -40,6 +41,7 @@ export function apiRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> })
   router.use('/registrations', registrationsRouter({ dbPool }));
   router.use('/groups', groupsRouter({ dbPool }));
   router.use('/registration-options', registrationOptionsRouter({ dbPool }));
+  router.use('/cycles', cyclesRouter({ dbPool }));
 
   return router;
 }
