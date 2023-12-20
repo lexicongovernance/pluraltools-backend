83cf0874b960ec1a7efeca05817dbe26181408af
Martin Benedikt Busch <bene.busch1992@gmail.com>
2023-12-20 09:38:41 +0100

    add options to cycles

--- src/db/options.ts
+++ src/db/options.ts
@@ -1,11 +1,11 @@
 import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
-import { cycles } from './cycles'; // Assuming your cycles file is in the same directory
+import { cycles } from './cycles';
 import { relations } from 'drizzle-orm';
 
 export const options = pgTable('options', {
   id: uuid('id').primaryKey().defaultRandom(),
-  cycleId: uuid('cycle_id')
-    .references(() => cycles.id) // Assuming cycles has an 'id' field
+  cycleId: uuid('id')
+    .references(() => cycles.id)
     .notNull(),
   text: varchar('text', { length: 256 }).notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
