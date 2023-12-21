9690f927c984cd2879df6e3acc2fbecd2ae0b6f4
Diego Alzate <diego35.da@gmail.com>
2023-12-21 10:21:59 -0500

    Adds votes table (#31)

---
+++ src/db/votes.ts
@@ -0,0 +1,30 @@
+import { integer, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { users } from './users';
+import { relations } from 'drizzle-orm';
+import { options } from './options';
+
+export const votes = pgTable('votes', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
+  optionId: uuid('option_id')
+    .notNull()
+    .references(() => options.id),
+  numOfVotes: integer('num_of_votes').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const votesRelations = relations(votes, ({ one }) => ({
+  user: one(users, {
+    fields: [votes.userId],
+    references: [users.id],
+  }),
+  option: one(options, {
+    fields: [votes.optionId],
+    references: [options.id],
+  }),
+}));
+
+export type Vote = typeof votes.$inferSelect;
