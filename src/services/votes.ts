8cf05f5412d052a20b8926b2a252893fc7b58cea
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-01-02 16:55:18 +0100

    save vote count in options table

--- src/services/votes.ts
+++ src/services/votes.ts
@@ -33,6 +33,19 @@ export function saveVote(dbPool: PostgresJsDatabase<typeof db>) {
     // Extract the list of numOfVotes from the result
     const numOfVotesList = voteList.map((vote) => vote.numOfVotes);
 
+    // Sum the numOfVotesList (to be replaced by quadratic voting module)
+    const totalVotes = numOfVotesList.reduce((acc, votes) => acc + votes, 0);
+
+    // Update the options table with the new vote count
+    await dbPool
+      .update(db.options)
+      .set({
+        voteCount: totalVotes,
+        updatedAt: new Date(),
+      })
+      .where(eq(db.options.id, body.data.optionId))
+      .execute();
+
     // Return new vote object and list of numOfVotes for the optionId
     return res.json({ data: newVote[0], numOfVotesList });
   };
