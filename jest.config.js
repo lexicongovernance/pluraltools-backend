fa15154311ab13c5d9009dda6c91d3544c73c011
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2024-01-10 23:18:47 +0100

    fix jest depreciation warning (#61)

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    globals: {
-      'ts-jest': {
-        tsconfig: './tsconfig.json',
-      },
-    },
-    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-    testPathIgnorePatterns: ['node_modules'],
-    preset: 'ts-jest',
-  };
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.json' }],
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['node_modules'],
+  preset: 'ts-jest',
+};
