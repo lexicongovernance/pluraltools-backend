79e2d905ad3cda1e49fe475f36f60fc73522b592
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-01-11 09:43:49 +0100

    add possibility to add a custom number of hearts + tests

--- src/services/availableHearts.test.ts
+++ src/services/availableHearts.test.ts
@@ -11,6 +11,7 @@ describe('availableHearts function', () => {
     const result = availableHearts(numProposals, baseNumerator, baseDenominator, maxRatio);
     expect(result).toEqual(5);
   });
+
   test('error if max ratio was not calculated correctly', () => {
     const numProposals = 2;
     const baseNumerator = 4;
@@ -19,5 +20,32 @@ describe('availableHearts function', () => {
 
     const result = availableHearts(numProposals, baseNumerator, baseDenominator, maxRatio);
     expect(result).toBeNull();
+  });
+
+  test('returns custom hearts if customHearts is set', () => {
+    const numProposals = 2;
+    const baseNumerator = 4;
+    const baseDenominator = 5;
+    const maxRatio = 0.8;
+    const customHearts = 10;
+
+    const result = availableHearts(
+      numProposals,
+      baseNumerator,
+      baseDenominator,
+      maxRatio,
+      customHearts,
+    );
+    expect(result).toEqual(customHearts);
+  });
+
+  test('executes the function if customHearts is not set', () => {
+    const numProposals = 2;
+    const baseNumerator = 4;
+    const baseDenominator = 5;
+    const maxRatio = 0.8;
+
+    const result = availableHearts(numProposals, baseNumerator, baseDenominator, maxRatio);
+    expect(result).toEqual(5);
   });
 });
--- src/services/availableHearts.ts
+++ src/services/availableHearts.ts
@@ -3,6 +3,7 @@ export function availableHearts(
   baseNumerator: number,
   baseDenominator: number,
   maxRatio: number,
+  customHearts: number | null = null,
 ): number | null {
   // Calculates number of hearts that a participant has available. The underlying assumption of the calculation is
   // that a participant must assign at least one heart to each available proposal.
@@ -10,7 +11,12 @@ export function availableHearts(
   // :param: baseNumberator: specifies the minimum amounts of hearts a participant must allocate to a given proposal to satisfy the max ratio.
   // :param: baseDenominator: specifies the minimum amount of hearts a participant must have available to satisfy the max ratio.
   // :param: maxRatio: specifies the preference ratio a participant should be able to express over two project options.
+  // :param: customHearts: if this parameter is set then the function will return custom hearts independent of the number of projects.
   // :returns: number of a available heart for each participant given a number of proposals.
+
+  if (customHearts !== null) {
+    return customHearts;
+  }
 
   const maxVotes = baseNumerator + (numProposals - 2) * baseNumerator;
   const minHearts = baseDenominator + (numProposals - 2) * baseDenominator;
