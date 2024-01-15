630338bb680b32ad9e99f6a3d915aa514ed5c160
Diego Alzate <diego35.da@gmail.com>
2024-01-15 14:58:25 -0500

    Add getEventCycles endpoint to events router

--- src/routers/events.ts
+++ src/routers/events.ts
@@ -6,6 +6,7 @@ import { getEvents } from '../services/events';
 import { getRegistration, saveRegistration } from '../services/registrations';
 import { getRegistrationFields } from '../services/registrationFields';
 import { getRegistrationData } from '../services/registrationData';
+import { getEventCycles } from '../services/cycles';
 const router = express.Router();
 
 export function eventsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
@@ -14,5 +15,6 @@ export function eventsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db>
   router.get('/:eventId/registration-data', isLoggedIn(), getRegistrationData(dbPool));
   router.post('/:eventId/registration', isLoggedIn(), saveRegistration(dbPool));
   router.get('/:eventId/registration', isLoggedIn(), getRegistration(dbPool));
+  router.get('/:eventId/cycles', isLoggedIn(), getEventCycles(dbPool));
   return router;
 }
