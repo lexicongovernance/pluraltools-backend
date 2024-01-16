dd2bd1fdc866bd0e1be1bec7ffc84f679b133ae2
Diego Alzate <diego35.da@gmail.com>
2024-01-15 19:00:10 -0500

    Add getEvent endpoint to events router

--- src/routers/events.ts
+++ src/routers/events.ts
@@ -2,7 +2,7 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { default as express } from 'express';
 import type * as db from '../db';
 import { isLoggedIn } from '../middleware/isLoggedIn';
-import { getEvents } from '../services/events';
+import { getEvent, getEvents } from '../services/events';
 import { getRegistration, saveRegistration } from '../services/registrations';
 import { getRegistrationFields } from '../services/registrationFields';
 import { getRegistrationData } from '../services/registrationData';
@@ -11,6 +11,7 @@ const router = express.Router();
 
 export function eventsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
   router.get('/', isLoggedIn(), getEvents(dbPool));
+  router.get('/:eventId', isLoggedIn(), getEvent(dbPool));
   router.get('/:eventId/registration-fields', isLoggedIn(), getRegistrationFields(dbPool));
   router.get('/:eventId/registration-data', isLoggedIn(), getRegistrationData(dbPool));
   router.post('/:eventId/registration', isLoggedIn(), saveRegistration(dbPool));
