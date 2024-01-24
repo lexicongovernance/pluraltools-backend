ce6844812c2b0c0072575aab42b8440eeccf5cf4
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-01-24 14:48:50 +0100

    new database migration due to a breaking change

--- migrations/0002_tranquil_fallen_one.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "question_options" ADD COLUMN "description" varchar;--> statement-breakpoint
-ALTER TABLE "votes" ADD COLUMN "question_id" uuid NOT NULL;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "votes" ADD CONSTRAINT "votes_question_id_forum_questions_id_fk" FOREIGN KEY ("question_id") REFERENCES "forum_questions"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
