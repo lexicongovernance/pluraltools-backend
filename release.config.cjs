d1332f5e444700e83075f3199a291a290653e362
Diego Alzate <diego35.da@gmail.com>
2024-01-29 15:26:55 -0500

    Add deployment workflows for development and production environments

--- release.config.cjs
+++ release.config.cjs
@@ -14,7 +14,13 @@ module.exports = {
       },
     ],
     '@semantic-release/github',
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
   tagFormat: 'v${version}',
 };
