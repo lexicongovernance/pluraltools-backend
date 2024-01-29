02b061b35fde54d86f74fda418506c64b3892ba8
Diego Alzate <diego35.da@gmail.com>
2024-01-29 15:31:39 -0500

    Revert "Diego/automate releases" (#112)

--- release.config.cjs
+++ release.config.cjs
@@ -2,25 +2,11 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['main', 'develop', 'next'],
+  branches: ['main', 'next', '+([0-9])?(.{+([0-9]),x}).x'],
   repositoryUrl: 'https://github.com/lexicongovernance/forum-backend',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
   ],
-  tagFormat: 'v${version}',
 };
