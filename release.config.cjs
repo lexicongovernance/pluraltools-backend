5fadc72cbd0b6b5edf254c43c2dd0b300a2281f0
Diego Alzate <diego35.da@gmail.com>
2024-01-29 15:30:12 -0500

    Diego/automate releases (#111)
    * chore: adds commitizen
    
    * chore(automation): add better settings for semantic version release
    
    * Add deployment workflows for development and production environments

--- release.config.cjs
+++ release.config.cjs
@@ -2,11 +2,25 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['main', 'next', '+([0-9])?(.{+([0-9]),x}).x'],
+  branches: ['main', 'develop', 'next'],
   repositoryUrl: 'https://github.com/lexicongovernance/forum-backend',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
+  tagFormat: 'v${version}',
 };
