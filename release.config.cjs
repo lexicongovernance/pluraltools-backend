f37a890ce91461f85d6729067c129419866b4f4c
Diego Alzate <diego35.da@gmail.com>
2024-01-29 15:44:45 -0500

    Diego/hotfix versioning (#113)
    * add event registration description
    
    * add version bump
    
    * chore: adds commitizen
    
    * chore(automation): add better settings for semantic version release
    
    * Add deployment workflows for development and production environments
    
    * fix: package versioning fix
    
    * docs: release docs
    
    ---------
    
    Co-authored-by: Martin Benedikt Busch <bene.busch1992@gmail.com>
    Co-authored-by: Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>

--- release.config.cjs
+++ release.config.cjs
@@ -1,12 +1,27 @@
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
+// eslint-disable-next-line no-undef
 module.exports = {
-  branches: ['main', 'next', '+([0-9])?(.{+([0-9]),x}).x'],
+  branches: ['main', 'develop', 'next'],
   repositoryUrl: 'https://github.com/lexicongovernance/forum-backend',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
+  tagFormat: 'v${version}',
 };
