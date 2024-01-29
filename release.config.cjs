8d8ea22b82d4468de7e8ecf2fd3402b0c4e650ce
Diego Alzate <diego35.da@gmail.com>
2024-01-29 14:50:56 -0500

    chore(automation): add better settings for semantic version release

--- release.config.cjs
+++ release.config.cjs
@@ -2,11 +2,19 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['main', 'next', '+([0-9])?(.{+([0-9]),x}).x'],
+  branches: ['main', 'develop', 'next'],
   repositoryUrl: 'https://github.com/lexicongovernance/forum-backend',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     '@semantic-release/github',
+    '@semantic-release/git',
   ],
+  tagFormat: 'v${version}',
 };
