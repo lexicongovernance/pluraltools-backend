4450bc305abae9eb5e6b63091b07062b32e58944
Diego Alzate <diego35.da@gmail.com>
2024-01-29 15:38:45 -0500

    fix: package versioning fix

--- release.config.cjs
+++ release.config.cjs
@@ -1,6 +1,7 @@
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
+// eslint-disable-next-line no-undef
 module.exports = {
   branches: ['main', 'develop', 'next'],
   repositoryUrl: 'https://github.com/lexicongovernance/forum-backend',
