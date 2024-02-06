f4008509d5f000c28b16e010403d77a4cc246262
Diego Alzate <diego35.da@gmail.com>
2024-02-06 09:41:29 -0500

    Improve zupass login (#146)
    * Refactor authentication logic and add user verification schema
    
    * Remove unused createNonce function
    
    * Update auth router and service to use verifyPCD instead of verifyNonce and simplify function
    
    * fix: removes possible impersonation hack

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,13 +1,12 @@
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import type * as db from '../db';
 import { default as express } from 'express';
-import { createNonce, verifyNonce } from '../services/auth/zupass';
+import { verifyPCD } from '../services/auth/zupass';
 import { destroySession } from '../services/auth';
 const router = express.Router();
 
 export function authRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
-  router.get('/zupass/nonce', createNonce());
-  router.post('/zupass/verify', verifyNonce(dbPool));
+  router.post('/zupass/verify', verifyPCD(dbPool));
   router.post('/logout', destroySession());
   return router;
 }
