c04034d4927427686bdd14db6ac2ac20f868f400
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-02-23 16:00:42 +0100

    remove console log

--- src/services/resultsPage.ts
+++ src/services/resultsPage.ts
@@ -212,8 +212,6 @@ export function getResultStatistics(dbPool: PostgresJsDatabase<typeof db>) {
         optionStats: indivStats,
       };
 
-      console.log('Response from getResultStatistics:', responseData);
-
       return res.status(200).json({ data: responseData });
     } catch (error) {
       console.error('Error in getResultStatistics:', error);
