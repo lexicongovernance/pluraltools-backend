3d3d42432a3d6a6b50224ec5d2642a15ee12ad07
Diego Alzate <diego35.da@gmail.com>
2024-02-27 20:56:07 -0500

    Add getUserOptions endpoint to users router (#214)
    * Add getUserOptions endpoint to users router
    
    * Refactor left join in getUserOptions function

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -4,6 +4,7 @@ import type * as db from '../db';
 import { getUser, getUserAttributes, updateUser } from '../services/users';
 import { getGroupsPerUser } from '../services/groups';
 import { isLoggedIn } from '../middleware/isLoggedIn';
+import { getUserOptions } from '../services/options';
 const router = express.Router();
 
 export function usersRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
@@ -11,5 +12,6 @@ export function usersRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> 
   router.put('/:userId', isLoggedIn(), updateUser(dbPool));
   router.get('/:userId/groups', isLoggedIn(), getGroupsPerUser(dbPool));
   router.get('/:userId/attributes', isLoggedIn(), getUserAttributes(dbPool));
+  router.get('/:userId/options', isLoggedIn(), getUserOptions(dbPool));
   return router;
 }
--- src/services/options.ts
+++ src/services/options.ts
@@ -18,3 +18,23 @@ export function getOption(dbPool: PostgresJsDatabase<typeof db>) {
     return res.json({ data: option });
   };
 }
+
+export function getUserOptions(dbPool: PostgresJsDatabase<typeof db>) {
+  return async function (req: Request, res: Response) {
+    const { userId } = req.params;
+
+    if (!userId) {
+      return res.status(400).json({ error: 'Missing userId' });
+    }
+
+    const query = await dbPool
+      .select()
+      .from(db.questionOptions)
+      .leftJoin(db.registrations, eq(db.registrations.id, db.questionOptions.registrationId))
+      .where(eq(db.registrations.userId, userId));
+
+    const options = query.map((q) => q.question_options);
+
+    return res.json({ data: options });
+  };
+}
