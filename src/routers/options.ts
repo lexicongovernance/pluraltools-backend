a1fe7f26bf6caffd80f72e860c0074adac3a15b9
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2024-03-01 15:10:08 +0100

    Delete comment functionality (#230)
    * implement delete comment function
    
    * add endpoint to option router + docstring
    
    * add commentId to router

--- src/routers/options.ts
+++ src/routers/options.ts
@@ -3,7 +3,7 @@ import { default as express } from 'express';
 import type * as db from '../db';
 import { isLoggedIn } from '../middleware/isLoggedIn';
 import { getOption } from '../services/options';
-import { saveComment, getCommentsForOption } from '../services/comments';
+import { saveComment, getCommentsForOption, deleteComment } from '../services/comments';
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ export function optionsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db
   router.get('/:optionId', isLoggedIn(dbPool), getOption(dbPool));
   router.get('/:optionId/comments', isLoggedIn(dbPool), getCommentsForOption(dbPool));
   router.post('/:optionId/comments', isLoggedIn(dbPool), saveComment(dbPool));
+  router.delete('/:optionId/comments/:commentId', isLoggedIn(dbPool), deleteComment(dbPool));
+
   return router;
 }
