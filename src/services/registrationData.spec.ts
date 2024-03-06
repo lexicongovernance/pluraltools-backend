8245556242afbf1d4eb0c608d80e30662e239518
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-03-06 11:18:10 +0100

    add tests for upsert registration data

--- src/services/registrationData.spec.ts
+++ src/services/registrationData.spec.ts
@@ -3,7 +3,6 @@ import postgres from 'postgres';
 import * as db from '../db';
 import { createDbPool } from '../utils/db/createDbPool';
 import { runMigrations } from '../utils/db/runMigrations';
-import { registrationDataSchema } from '../types';
 import { insertRegistrationSchema } from '../types';
 import { cleanup, seed } from '../utils/db/seed';
 import { z } from 'zod';
@@ -15,11 +14,11 @@ const DB_CONNECTION_URL = 'postgresql://postgres:secretpassword@localhost:5432';
 describe('service: registrationData', () => {
   let dbPool: PostgresJsDatabase<typeof db>;
   let dbConnection: postgres.Sql<NonNullable<unknown>>;
-  let registrationTestData: z.infer<typeof registrationDataSchema>;
   let questionOption: db.QuestionOption | undefined;
   let forumQuestion: db.ForumQuestion | undefined;
   let registrationField: db.RegistrationField | undefined;
   let user: db.User | undefined;
+  let registration: db.Registration | undefined;
   let testRegistration: z.infer<typeof insertRegistrationSchema>;
 
   beforeAll(async () => {
@@ -35,12 +34,6 @@ describe('service: registrationData', () => {
     forumQuestion = forumQuestions[0];
     user = users[0];
     registrationField = registrationFields[0];
-    registrationTestData = [
-      {
-        registrationFieldId: registrationField?.id ?? '',
-        value: 'something',
-      },
-    ];
 
     testRegistration = {
       userId: users[0]?.id ?? '',
@@ -55,10 +48,45 @@ describe('service: registrationData', () => {
     };
 
     // Add test registration data to the db
-    await sendRegistrationData(dbPool, testRegistration, testRegistration.userId);
+    registration = await sendRegistrationData(dbPool, testRegistration, testRegistration.userId);
   });
 
-  test('should return aggregated statistics when all queries return valid data', async () => {});
+  test('should update existing records', async () => {
+    // Call the function with registration ID and registration data to update
+    const registrationId = registration?.id ?? '';
+    const registrationFieldId = registrationField?.id ?? '';
+    const updatedValue = 'updated';
+
+    console.log('testRegistration', testRegistration);
+    const registrationTestData = [
+      {
+        registrationFieldId: registrationFieldId,
+        value: updatedValue,
+      },
+    ];
+
+    const updatedData = await upsertRegistrationData({
+      dbPool,
+      registrationId: registrationId,
+      registrationData: registrationTestData,
+    });
+    console.log('updatedData', updatedData);
+
+    // Assert that the updated data array is not empty
+    expect(updatedData).toBeDefined();
+    expect(updatedData).not.toBeNull();
+
+    if (updatedData) {
+      // Assert that the updated data has the correct structure
+      expect(updatedData.length).toBeGreaterThan(0);
+      expect(updatedData[0]).toHaveProperty('id');
+      expect(updatedData[0]).toHaveProperty('registrationId', registrationId);
+      expect(updatedData[0]).toHaveProperty('registrationFieldId', registrationFieldId);
+      expect(updatedData[0]).toHaveProperty('value', updatedValue);
+      expect(updatedData[0]).toHaveProperty('createdAt');
+      expect(updatedData[0]).toHaveProperty('updatedAt');
+    }
+  });
 
   afterAll(async () => {
     await cleanup(dbPool);
