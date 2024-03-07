160d3825f3d9ebdef8c251150d50516a721b8794
Diego Alzate <diego35.da@gmail.com>
2024-03-07 11:15:34 -0500

    Update user service to check for existing email and username (#247)
    * Update user service to check for existing email and username
    
    * Refactor insertUserSchema to handle empty strings as null values

--- src/types/users.ts
+++ src/types/users.ts
@@ -5,7 +5,17 @@ import { users } from '../db';
 export const groupIdsSchema = z.string().array();
 export const userAttributesSchema = z.record(z.string());
 
-export const insertUserSchema = createInsertSchema(users).extend({
-  groupIds: groupIdsSchema,
-  userAttributes: userAttributesSchema,
-});
+export const insertUserSchema = createInsertSchema(users)
+  .extend({
+    groupIds: groupIdsSchema,
+    userAttributes: userAttributesSchema,
+  })
+  .transform((data) => {
+    // make empty strings null
+    return {
+      ...data,
+      email: data.email || null,
+      username: data.username || null,
+      name: data.name || null,
+    };
+  });
