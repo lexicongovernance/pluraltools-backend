80a1c3f1394cff2480edc060db1bbfa6dff293b5
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-03-12 10:55:00 +0100

    setup group categories table

---
+++ src/db/groupCategories.ts
@@ -0,0 +1,17 @@
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { events } from './events';
+import { relations } from 'drizzle-orm';
+
+export const groupCategories = pgTable('group_category', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    groupCategoryLabel: varchar('group_category_label'),
+    eventId: uuid('event_id').references(() => events.id).notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const groupCategoriesRelations = relations(groupCategories, ({ many }) => ({
+    event: many(events),
+}));
+
+export type GroupCategory = typeof groupCategories.$inferSelect;
