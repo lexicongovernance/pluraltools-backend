67892b231569fa87000f863bc5fe1243a4fdf376
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2024-03-13 16:29:12 +0100

    Group categories table (#265)
    * setup group categories table
    
    * add group category label to registration fields
    
    * add migrations
    
    * add user id to registration data
    
    * include user id in upsertRegistrationData
    
    * make labels shorter
    
    * revert changes and add group label id to groups
    
    * change table name

---
+++ src/db/groupCategories.ts
@@ -0,0 +1,22 @@
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { events } from './events';
+import { groups } from './groups';
+import { relations } from 'drizzle-orm';
+
+export const groupCategories = pgTable('group_categories', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  groupLabel: varchar('group_label'),
+  eventId: uuid('event_id').references(() => events.id),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const groupCategoriesRelations = relations(groupCategories, ({ one, many }) => ({
+  event: one(events, {
+    fields: [groupCategories.eventId],
+    references: [events.id],
+  }),
+  group: many(groups),
+}));
+
+export type GroupCategory = typeof groupCategories.$inferSelect;
