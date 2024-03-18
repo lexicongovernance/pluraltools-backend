31412ec5a08c301db2dd9b20bbcb22799d4deaa8
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-03-18 15:10:04 +0100

    simplify routers

--- src/routers/groups.ts
+++ src/routers/groups.ts
@@ -1,16 +1,11 @@
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { default as express } from 'express';
 import type * as db from '../db';
-import { getGroups, getGroupsByCategoryId } from '../services/groups';
+import { getGroups } from '../services/groups';
 import { isLoggedIn } from '../middleware/isLoggedIn';
 const router = express.Router();
 
 export function groupsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
   router.get('/', isLoggedIn(dbPool), getGroups(dbPool));
-  router.get(
-    '/group-categories/:groupCategoryId',
-    isLoggedIn(dbPool),
-    getGroupsByCategoryId(dbPool),
-  );
   return router;
 }
