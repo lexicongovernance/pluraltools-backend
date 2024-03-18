1ae1452ae216589fb101679c1ff9c1bd8f7ba74a
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-03-18 14:53:15 +0100

    router changes

--- src/routers/groups.ts
+++ src/routers/groups.ts
@@ -1,23 +1,16 @@
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { default as express } from 'express';
 import type * as db from '../db';
-import {
-  getGroups,
-  getGroupsByCategoryId,
-  getGroupsPerUser,
-  getGroupsPerUserByCategoryId,
-} from '../services/groups';
+import { getGroups, getGroupsByCategoryId } from '../services/groups';
 import { isLoggedIn } from '../middleware/isLoggedIn';
 const router = express.Router();
 
 export function groupsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
-  router.get('/groups', isLoggedIn(dbPool), getGroups(dbPool));
-  router.get('/groups/:groupCategoryId', isLoggedIn(dbPool), getGroupsByCategoryId(dbPool));
-  router.get('/groups/:userId', isLoggedIn(dbPool), getGroupsPerUser(dbPool));
+  router.get('/', isLoggedIn(dbPool), getGroups(dbPool));
   router.get(
-    '/groups/:userId/:groupCategoryId',
+    '/group-categories/:groupCategoryId',
     isLoggedIn(dbPool),
-    getGroupsPerUserByCategoryId(dbPool),
+    getGroupsByCategoryId(dbPool),
   );
   return router;
 }
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -2,7 +2,7 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { default as express } from 'express';
 import type * as db from '../db';
 import { getUser, getUserAttributes, updateUser } from '../services/users';
-import { getGroupsPerUser } from '../services/groups';
+import { getGroupsPerUser, getGroupsPerUserByCategoryId } from '../services/groups';
 import { isLoggedIn } from '../middleware/isLoggedIn';
 import { getUserOptions } from '../services/options';
 import { getUserRegistrations } from '../services/registrations';
@@ -12,6 +12,11 @@ export function usersRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> 
   router.get('/', isLoggedIn(dbPool), getUser(dbPool));
   router.put('/:userId', isLoggedIn(dbPool), updateUser(dbPool));
   router.get('/:userId/groups', isLoggedIn(dbPool), getGroupsPerUser(dbPool));
+  router.get(
+    '/:userId/group-categories/:groupCategoryId/groups',
+    isLoggedIn(dbPool),
+    getGroupsPerUserByCategoryId(dbPool),
+  );
   router.get('/:userId/attributes', isLoggedIn(dbPool), getUserAttributes(dbPool));
   router.get('/:userId/options', isLoggedIn(dbPool), getUserOptions(dbPool));
   router.get('/:userId/registrations', isLoggedIn(dbPool), getUserRegistrations(dbPool));
