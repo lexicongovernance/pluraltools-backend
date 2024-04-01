db903f9912d82d5dc65a28fef6128c24c5f46a3b
Diego Alzate <diego35.da@gmail.com>
2024-04-01 10:00:41 -0500

    Add user schema test and update insertUserSchema

---
+++ src/services/users.spec.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+import { insertUserSchema } from '../types/users';
+
+describe('service: users', function () {
+  describe('schema: insertUserSchema', function () {
+    it('should remove empty strings from user data', function () {
+      const user: z.infer<typeof insertUserSchema> = {
+        email: '',
+        username: '',
+        firstName: '',
+        lastName: '',
+        telegram: '',
+        groupIds: ['1'],
+        userAttributes: {},
+      };
+
+      const transformedUser: { [key: string]: string | null | string[] | object } =
+        insertUserSchema.parse(user);
+
+      // loop through all keys and check if they are not empty strings
+
+      for (const key of Object.keys(transformedUser)) {
+        console.log(key);
+        expect(transformedUser[key]).not.toBe('');
+      }
+    });
+  });
+});
