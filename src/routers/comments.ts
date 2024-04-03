6453239b2e3f1fb87b1f761fa882a87b642a8691
Diego Alzate <diego35.da@gmail.com>
2024-04-03 18:51:15 -0500

    Refactor comment handling and add option handlers

--- src/routers/comments.ts
+++ src/routers/comments.ts
@@ -3,13 +3,17 @@ import { default as express } from 'express';
 import type * as db from '../db';
 import { isLoggedIn } from '../middleware/isLoggedIn';
 import {
+  deleteCommentHandler,
   deleteCommentLikeHandler,
   getCommentLikesHandler,
+  saveCommentHandler,
   saveCommentLikeHandler,
 } from '../handlers/comments';
 const router = express.Router();
 
 export function commentsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
+  router.post('/', isLoggedIn(dbPool), saveCommentHandler(dbPool));
+  router.delete('/:commentId', isLoggedIn(dbPool), deleteCommentHandler(dbPool));
   router.get('/:commentId/likes', isLoggedIn(dbPool), getCommentLikesHandler(dbPool));
   router.post('/:commentId/likes', isLoggedIn(dbPool), saveCommentLikeHandler(dbPool));
   router.delete('/:commentId/likes', isLoggedIn(dbPool), deleteCommentLikeHandler(dbPool));
--- src/services/comments.ts
+++ src/services/comments.ts
@@ -1,39 +1,8 @@
 import { eq, and } from 'drizzle-orm';
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import type { Request, Response } from 'express';
 import { insertCommentSchema } from '../types';
 import { z } from 'zod';
 import * as db from '../db';
-
-/**
- * Saves a comment to the database.
- * @param {PostgresJsDatabase<typeof db>} dbPool - The database pool connection.
- * @returns {Promise<void>} - A promise that resolves once the comment is saved.
- */
-export function saveComment(dbPool: PostgresJsDatabase<typeof db>) {
-  return async function (req: Request, res: Response) {
-    const userId = req.session.userId;
-    const body = insertCommentSchema.safeParse(req.body);
-
-    if (!body.success) {
-      return res.status(400).json({ errors: body.error.issues });
-    }
-
-    const canComment = await userCanComment(dbPool, userId, body.data.questionOptionId);
-
-    if (!canComment) {
-      return res.status(403).json({ errors: [{ message: 'User cannot comment on this option' }] });
-    }
-
-    try {
-      const out = await insertComment(dbPool, body.data, userId);
-      return res.json({ data: out });
-    } catch (e) {
-      console.log('error saving comment ' + e);
-      return res.sendStatus(500);
-    }
-  };
-}
 
 /**
  * Inserts a new comment into the database.
@@ -43,7 +12,7 @@ export function saveComment(dbPool: PostgresJsDatabase<typeof db>) {
  * @returns {Promise<Comment>} - A promise that resolves with the inserted comment.
  * @throws {Error} - Throws an error if the insertion fails.
  */
-export async function insertComment(
+export async function saveComment(
   dbPool: PostgresJsDatabase<typeof db>,
   data: z.infer<typeof insertCommentSchema>,
   userId: string,
@@ -70,79 +39,64 @@ export async function insertComment(
  * @returns {Promise<void>} - A promise that resolves once the comment and associated likes are deleted.
  * @throws {Error} - Throws an error if the deletion fails.
  */
-export function deleteComment(dbPool: PostgresJsDatabase<typeof db>) {
-  return async function (req: Request, res: Response) {
-    const commentId = req.params.commentId;
-    const userId = req.session.userId;
+export async function deleteComment(
+  dbPool: PostgresJsDatabase<typeof db>,
+  data: {
+    commentId: string;
+    userId: string;
+  },
+): Promise<{ errors?: string[]; data?: db.Comment }> {
+  const { commentId, userId } = data;
 
-    if (!commentId) {
-      return res.status(400).json({ errors: ['commentId is required'] });
-    }
+  // Only the author of the comment has the authorization to delete the comment
+  const comment = await dbPool.query.comments.findFirst({
+    where: and(eq(db.comments.id, commentId), eq(db.comments.userId, userId)),
+  });
 
-    try {
-      // Only the author of the comment has the authorization to delete the comment
-      const comment = await dbPool.query.comments.findFirst({
-        where: and(eq(db.comments.id, commentId), eq(db.comments.userId, userId)),
-      });
+  if (!comment) {
+    return { errors: ['Unauthorized to delete comment'] };
+  }
 
-      if (!comment) {
-        return res.status(404).json({ errors: ['Unauthorized to delete comment'] });
-      }
+  // Delete all likes associated with the deleted comment
+  await dbPool.delete(db.likes).where(eq(db.likes.commentId, commentId));
 
-      // Delete all likes associated with the deleted comment
-      await dbPool.delete(db.likes).where(eq(db.likes.commentId, commentId));
+  // Delete the comment
+  const deletedComment = await dbPool
+    .delete(db.comments)
+    .where(eq(db.comments.id, commentId))
+    .returning();
 
-      // Delete the comment
-      const deletedComment = await dbPool
-        .delete(db.comments)
-        .where(eq(db.comments.id, commentId))
-        .returning();
-
-      return res.json({ data: deletedComment });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errors: ['Failed to delete comment'] });
-    }
-  };
+  return { data: deletedComment[0] };
 }
 
-/**
- * Retrieves comments related to a specific question option from the database and associates them with corresponding user information.
- * @param {PostgresJsDatabase<typeof db>} dbPool - The database pool connection.
- * @returns {Promise<void>} - A promise that resolves with the retrieved comments, each associated with user information if available.
- */
-export function getCommentsForOption(dbPool: PostgresJsDatabase<typeof db>) {
-  return async function (req: Request, res: Response) {
-    const optionId = req.params.optionId ?? '';
+export async function getOptionComments(
+  dbPool: PostgresJsDatabase<typeof db>,
+  data: {
+    optionId: string;
+  },
+) {
+  // Query comments
+  const rows = await dbPool
+    .select()
+    .from(db.comments)
+    .leftJoin(db.users, eq(db.comments.userId, db.users.id))
+    .where(eq(db.comments.questionOptionId, data.optionId));
 
-    try {
-      // Query comments
-      const rows = await dbPool
-        .select()
-        .from(db.comments)
-        .leftJoin(db.users, eq(db.comments.userId, db.users.id))
-        .where(eq(db.comments.questionOptionId, optionId));
+  const commentsWithUserNames = rows.map((row) => {
+    return {
+      id: row.comments.id,
+      userId: row.comments.userId,
+      questionOptionId: row.comments.questionOptionId,
+      value: row.comments.value,
+      createdAt: row.comments.createdAt,
+      user: {
+        id: row.users?.id,
+        username: row.users?.username,
+      },
+    };
+  });
 
-      const commentsWithUserNames = rows.map((row) => {
-        return {
-          id: row.comments.id,
-          userId: row.comments.userId,
-          questionOptionId: row.comments.questionOptionId,
-          value: row.comments.value,
-          createdAt: row.comments.createdAt,
-          user: {
-            id: row.users?.id,
-            username: row.users?.username,
-          },
-        };
-      });
-
-      return res.json({ data: commentsWithUserNames });
-    } catch (error) {
-      console.error('Error getting comments: ', error);
-      return res.sendStatus(500);
-    }
-  };
+  return commentsWithUserNames;
 }
 
 /**
@@ -152,7 +106,7 @@ export function getCommentsForOption(dbPool: PostgresJsDatabase<typeof db>) {
  * @param {string | undefined | null} optionId - The ID of the option for which the user is attempting to comment.
  * @returns {Promise<boolean>} A promise that resolves to true if the user can comment, false otherwise.
  */
-async function userCanComment(
+export async function userCanComment(
   dbPool: PostgresJsDatabase<typeof db>,
   userId: string,
   optionId: string | undefined | null,
