21dc9cb5fc332497a4255e3521a240db10016189
Diego Alzate <diego35.da@gmail.com>
2024-04-10 12:15:13 -0500

    remove migration

--- migrations/0015_brief_prodigy.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "registrations" ADD COLUMN "group_id" uuid;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "registrations" ADD CONSTRAINT "registrations_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "groups"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
