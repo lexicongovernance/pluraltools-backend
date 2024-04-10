54031e8b63a1a121fbe2a349ec4521f0c85e53e6
Diego Alzate <diego35.da@gmail.com>
2024-04-10 12:17:05 -0500

    merge migrations

--- migrations/0015_shiny_black_knight.sql
+++ migrations/0015_shiny_black_knight.sql
@@ -6,6 +6,13 @@ CREATE TABLE IF NOT EXISTS "questions_to_group_categories" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+ALTER TABLE "registrations" ADD COLUMN "group_id" uuid;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "registrations" ADD CONSTRAINT "registrations_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "groups"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "questions_to_group_categories" ADD CONSTRAINT "questions_to_group_categories_question_id_forum_questions_id_fk" FOREIGN KEY ("question_id") REFERENCES "forum_questions"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
