ca2b7ee0b725b99cfa48a73659ac4807acfcfa65
Diego Alzate <diego35.da@gmail.com>
2024-04-11 12:30:28 -0500

    add handler to router

--- src/routers/usersToGroups.ts
+++ src/routers/usersToGroups.ts
@@ -2,10 +2,11 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { default as express } from 'express';
 import type * as db from '../db';
 import { isLoggedIn } from '../middleware/isLoggedIn';
+import { joinGroupsHandler } from '../handlers/usersToGroups';
 
 const router = express.Router();
 
 export function usersToGroupsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
-  router.post('/', isLoggedIn(dbPool));
+  router.post('/', isLoggedIn(dbPool), joinGroupsHandler(dbPool));
   return router;
 }
