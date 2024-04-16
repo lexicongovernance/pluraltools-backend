eaa2460bcccc84388f4c97fdd2fa0e1bb10709c9
Diego Alzate <diego35.da@gmail.com>
2024-04-16 09:38:54 -0500

    add title, description and link to alerts

--- src/db/alerts.ts
+++ src/db/alerts.ts
@@ -1,9 +1,12 @@
-import { boolean, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const alerts = pgTable('alerts', {
   id: uuid('id').primaryKey().defaultRandom(),
-  startAt: timestamp('start_at').notNull(),
-  endAt: timestamp('end_at').notNull(),
+  title: varchar('title', { length: 256 }).notNull(),
+  description: varchar('description', { length: 1024 }),
+  link: varchar('link', { length: 256 }),
+  startAt: timestamp('start_at'),
+  endAt: timestamp('end_at'),
   active: boolean('active').default(false),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
--- src/routers/alerts.ts
+++ src/routers/alerts.ts
@@ -2,9 +2,10 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { default as express } from 'express';
 import type * as db from '../db';
 import { getActiveAlerts } from '../handlers/alerts';
+import { isLoggedIn } from '../middleware/isLoggedIn';
 const router = express.Router();
 
 export function alertsRouter({ dbPool }: { dbPool: PostgresJsDatabase<typeof db> }) {
-  router.get('/', getActiveAlerts(dbPool));
+  router.get('/', isLoggedIn(dbPool), getActiveAlerts(dbPool));
   return router;
 }
