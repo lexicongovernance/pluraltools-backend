5a351367f1ca5bd0f89cc2ba39df1fae9aaa8978
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-05-13 12:11:52 +0200

    fix tests

--- src/services/comments.spec.ts
+++ src/services/comments.spec.ts
@@ -1,8 +1,8 @@
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as db from '../db';
-import { createDbPool } from '../utils/db/createDbPool';
-import { runMigrations } from '../utils/db/runMigrations';
+import { createDbPool } from '../utils/db/create-db-pool';
+import { runMigrations } from '../utils/db/run-migrations';
 import { insertSimpleRegistrationSchema } from '../types';
 import { cleanup, seed } from '../utils/db/seed';
 import { z } from 'zod';
