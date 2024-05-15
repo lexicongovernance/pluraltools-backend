346a0aa7fb610f171279b96bf8f2867fcd2d94b4
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-05-15 12:25:52 +0200

    fix get option users

--- src/handlers/options.ts
+++ src/handlers/options.ts
@@ -76,7 +76,7 @@ export function getOptionUsersHandler(dbPool: PostgresJsDatabase<typeof db>) {
       // Send response
       return res.status(200).json({ data: responseData });
     } catch (error) {
-      console.error('Error in getOptionAuthors:', error);
+      console.error('Error in getOptionUsers:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   };
