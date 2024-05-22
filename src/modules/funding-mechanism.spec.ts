55a89b1f661d33df80bbbd5699fc413d0413d6ef
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-05-22 12:52:18 +0200

    add backedn fixes

--- src/modules/funding-mechanism.spec.ts
+++ src/modules/funding-mechanism.spec.ts
@@ -61,6 +61,37 @@ describe('test funding mechanism', () => {
     expect(result).toEqual(expectedResult);
   });
 
+  test('Does still allocate funding even if nothing was allocated to someone with a higher score because of a too high budget', () => {
+    const availableFunding = 15000;
+    const maxFunding = 12000;
+    const getOptionData = [
+      {
+        id: 'ID1',
+        voteScore: '5.5',
+        fundingRequest: '2000',
+      },
+      {
+        id: 'ID2',
+        voteScore: '6',
+        fundingRequest: '5000',
+      },
+      {
+        id: 'ID3',
+        voteScore: '8',
+        fundingRequest: '12000',
+      },
+    ];
+
+    const expectedResult = {
+      allocated_funding: { ID1: 2000, ID2: 0, ID3: 12000 },
+      remaining_funding: 1000,
+    };
+
+    const result = allocateFunding(availableFunding, maxFunding, getOptionData);
+    console.log(result);
+    expect(result).toEqual(expectedResult);
+  });
+
   test('Excludes projects from funding who specify more than the maximum amount', () => {
     const availableFunding = 15000;
     const maxFunding = 10000;
