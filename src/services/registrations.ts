9ecdeffb093c77e9b94bf3caa52e3251344cee78
Diego Alzate <diego35.da@gmail.com>
2024-07-10 14:01:45 +0100

    Add api routes for options (#443)
    * initial event registration changes
    
    * get validation working on event registrations
    
    * fix handler validation
    
    * add new fields to options and questions
    
    * add linking group to option
    
    * fix tests
    
    * create api endpoints for users to create options
    
    * add userCanCreate to questions table
    
    * add api endpoints for options
    
    * fix tests
    
    * fix migrations
    
    * fix drizzle journal
    
    * remove unused dependency
    
    * fix wrong registration name in wrong service

--- src/services/registrations.ts
+++ src/services/registrations.ts
@@ -1,66 +1,28 @@
 import { and, eq } from 'drizzle-orm';
-import { z } from 'zod';
-import { insertRegistrationSchema, fieldsSchema } from '../types';
-import * as db from '../db';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { z } from 'zod';
+import * as db from '../db';
+import { fieldsSchema, insertRegistrationSchema } from '../types';
 import { enforceRules } from './validation';
 
-export async function validateCreateRegistrationAuthorization({
-  dbPool,
-  userId,
-  groupId,
-}: {
-  dbPool: NodePgDatabase<typeof db>;
-  userId: string;
-  groupId?: string | null;
-}) {
-  if (groupId) {
-    const userGroup = await dbPool.query.usersToGroups.findFirst({
-      where: and(eq(db.usersToGroups.userId, userId), eq(db.usersToGroups.groupId, groupId)),
-    });
-
-    if (!userGroup) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-export async function validateUpdateRegistrationAuthorization({
+export async function getUserRegistration({
   dbPool,
   registrationId,
   userId,
-  groupId,
 }: {
   dbPool: NodePgDatabase<typeof db>;
   userId: string;
   registrationId: string;
-  groupId?: string | null;
-}) {
+}): Promise<db.Registration | null> {
   const existingRegistration = await dbPool.query.registrations.findFirst({
     where: and(eq(db.registrations.userId, userId), eq(db.registrations.id, registrationId)),
   });
 
   if (!existingRegistration) {
-    return false;
+    return null;
   }
 
-  if (existingRegistration.userId !== userId) {
-    return false;
-  }
-
-  if (groupId) {
-    const userGroup = await dbPool.query.usersToGroups.findFirst({
-      where: and(eq(db.usersToGroups.userId, userId), eq(db.usersToGroups.groupId, groupId)),
-    });
-
-    if (!userGroup) {
-      return false;
-    }
-  }
-
-  return true;
+  return existingRegistration;
 }
 
 export async function validateEventFields({
@@ -126,23 +88,13 @@ export async function saveRegistration(
 export async function updateRegistration({
   data,
   dbPool,
-  registrationId,
-  userId,
+  registration,
 }: {
   dbPool: NodePgDatabase<typeof db>;
   data: z.infer<typeof insertRegistrationSchema>;
-  registrationId: string;
-  userId: string;
+  registration: db.Registration;
 }) {
-  const existingRegistration = await dbPool.query.registrations.findFirst({
-    where: and(eq(db.registrations.userId, userId), eq(db.registrations.id, registrationId)),
-  });
-
-  if (!existingRegistration) {
-    throw new Error('registration not found');
-  }
-
-  const updatedRegistration = await updateRegistrationInDB(dbPool, existingRegistration, data);
+  const updatedRegistration = await updateRegistrationInDB(dbPool, registration, data);
 
   if (!updatedRegistration) {
     throw new Error('failed to save registration');
@@ -189,4 +141,36 @@ async function updateRegistrationInDB(
     .where(eq(db.registrations.id, registration.id))
     .returning();
   return updatedRegistration[0];
+}
+
+export async function validateRegistrationData({
+  registration,
+  dbPool,
+}: {
+  dbPool: NodePgDatabase<typeof db>;
+  registration: z.infer<typeof insertRegistrationSchema>;
+}) {
+  const rows = await dbPool.select().from(db.events).where(eq(db.events.id, registration.eventId));
+
+  if (!rows.length) {
+    return [];
+  }
+
+  const event = rows[0];
+
+  if (!event) {
+    return [];
+  }
+
+  // get fields for the event
+  const eventFields = fieldsSchema.safeParse(event.fields);
+
+  if (!eventFields.success) {
+    return [];
+  }
+
+  return enforceRules({
+    data: registration.data,
+    fields: eventFields.data,
+  });
 }
