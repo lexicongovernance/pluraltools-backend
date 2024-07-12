b9a48883186a812b5eeede6bd236bd2f49a043b1
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2024-07-12 11:26:11 +0200

    update test coverage (#445)
    * add tests for the user service
    
    * fix linting
    
    * test funding mechanism and refactor service
    
    * resolve lint warnings

--- src/services/users.ts
+++ src/services/users.ts
@@ -11,7 +11,7 @@ import { NodePgDatabase } from 'drizzle-orm/node-postgres';
  * @param {UserData} userData - The user data to check.
  * @returns {Promise<Array<string> | null>} - An array of errors if user data conflicts, otherwise null.
  */
-async function validateUserData(
+export async function validateUserData(
   dbPool: NodePgDatabase<typeof db>,
   userId: string,
   userData: UserData,
@@ -51,7 +51,7 @@ async function validateUserData(
  * @param {string} userId - The ID of the user to update.
  * @param {UserData} userData - The updated user data.
  */
-async function upsertUserData(
+export async function upsertUserData(
   dbPool: NodePgDatabase<typeof db>,
   userId: string,
   userData: UserData,
