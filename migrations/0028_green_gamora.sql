9e862eda7d3f78c87cf5319e9eaaee445de07da3
Diego Alzate <diego35.da@gmail.com>
2024-07-25 10:52:52 +0100

    update fields structure to dict (#450)
    * update validation schema
    
    * update fields object to be dict and not array
    
    * fix tests

--- migrations/0028_green_gamora.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "options" RENAME COLUMN "accepted" TO "show";--> statement-breakpoint
-ALTER TABLE "events" ADD COLUMN "fields" jsonb DEFAULT '[]'::jsonb NOT NULL;--> statement-breakpoint
-ALTER TABLE "registrations" ADD COLUMN "data" jsonb;--> statement-breakpoint
-ALTER TABLE "questions" ADD COLUMN "fields" jsonb DEFAULT '[]'::jsonb NOT NULL;--> statement-breakpoint
-ALTER TABLE "options" ADD COLUMN "data" jsonb;
