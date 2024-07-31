2de0069e64b4b5c0dc5a83f7c4f235b34555e661
Diego Alzate <diego35.da@gmail.com>
2024-07-31 16:22:48 +0100

    filter global navlinks

--- src/handlers/nav-links.ts
+++ src/handlers/nav-links.ts
@@ -1,20 +1,23 @@
 import type { Request, Response } from 'express';
 import * as schema from '../db/schema';
-import { and, eq, gte, lte, or } from 'drizzle-orm';
+import { and, eq, gte, isNull, lte, or } from 'drizzle-orm';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { logger } from '../utils/logger';
 
 export function getActiveNavLinks(dbPool: NodePgDatabase<typeof schema>) {
   return async function (req: Request, res: Response) {
     try {
-      const alerts = await dbPool.query.navLinks.findMany({
-        where: or(
-          eq(schema.navLinks.active, true),
-          and(lte(schema.navLinks.startAt, new Date()), gte(schema.navLinks.endAt, new Date())),
+      const navLinks = await dbPool.query.navLinks.findMany({
+        where: and(
+          or(
+            eq(schema.navLinks.active, true),
+            and(lte(schema.navLinks.startAt, new Date()), gte(schema.navLinks.endAt, new Date())),
+          ),
+          isNull(schema.navLinks.eventId),
         ),
       });
 
-      return res.json({ data: alerts });
+      return res.json({ data: navLinks });
     } catch (e) {
       logger.error(`[ERROR] ${JSON.stringify(e)}`);
       return res.sendStatus(500);
